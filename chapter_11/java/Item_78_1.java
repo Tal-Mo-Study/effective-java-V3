9c0f5141d8fcd56f952f123d0b96040da83fbc96
shcheol <kian1523@naver.com>
2023-03-28 23:41:24 +0900

    8,9회차 스터디_shc

---
+++ chapter_11/java/Item_78_1.java
@@ -0,0 +1,22 @@
+package chapter_11.java;
+
+import java.util.concurrent.*;
+
+public class Item_78_1 {
+
+    private static boolean stopRequested;
+
+    public static void main(String[] args)
+            throws InterruptedException {
+        Thread backgroundThread = new Thread(() -> {
+            int i = 0;
+            while (!stopRequested)
+                i++;
+        });
+        backgroundThread.start();
+
+        TimeUnit.SECONDS.sleep(1);
+        stopRequested = true;
+    }
+}
+
---
+++ chapter_12/java/Period.java
@@ -0,0 +1,36 @@
+package chapter_12.java;
+import java.io.Serializable;
+import java.util.*;
+
+public class Period implements Serializable {
+    private final Date start;
+    private final Date end;
+
+    /**
+     * @param  start 시작 시각
+     * @param  end 종료 시각. 시작 시각보다 뒤여야 한다.
+     * @throws IllegalArgumentException 시작 시각이 종료 시각보다 늦을 때 발생한다.
+     * @throws NullPointerException start나 end가 null이면 발생한다.
+     */
+    public Period(Date start, Date end) {
+        this.start = new Date(start.getTime());
+        this.end   = new Date(end.getTime());
+
+        if (this.start.compareTo(this.end) > 0)
+            throw new IllegalArgumentException(
+                    this.start + "가 " + this.end + "보다 늦다.");
+    }
+
+    public Date start() {
+        return new Date(start.getTime());
+    }
+
+    public Date end() {
+        return new Date(end.getTime());
+    }
+
+    public String toString() {
+        return start + " - " + end;
+    }
+
+}
